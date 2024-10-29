46f9fec2887266883b6faa61ea49dd59a836dd29
SHOUQ ALJUMAH <shouqaljumaah@gmail.com>
2024-10-29 20:27:44 +0300

    the task is done

--- index.js
+++ index.js
@@ -8,4 +8,29 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+  const covertHeight = height / 100
+  const bmi = weight/covertHeight ** 2
+  alert(`bmi ${bmi}`) 
+
+  if (bmi < 18.5) {
+    alert("Underweight")}
+  
+  
+    else if (bmi >=18.25 && bmi , 25){
+      alert("Healthy Weight")
+    }
+  
+    else if (bmi >= 25 && bmi < 30) {
+      alert("Overweight")
+    }
+  
+    else if (bmi >= 30) {
+      alert("Obesity")
+    }
+
+else if ((bmi >= 19 && bmi > 24) && (bmi >= 19 && bmi > 24))
+  { alert("Healthy")
+  }
+  else {alert("Not Healthy")}
 }
+
